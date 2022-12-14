447c9a1d35b48e03a8633625b23b68d1f5ad6bf7
jeremie-H <jeremie-H@users.noreply.github.com>
2022-12-15 00:19:21 +0100

    ✨ 2022 - day13 - incredible optimisation

--- 2022/src/day13.rs
+++ 2022/src/day13.rs
@@ -27,10 +27,8 @@ pub fn part2(input: &str) -> Result<i64, Box<dyn Error>> {
     input.push_str("\n[[6]]");
     let mut ordered_signals = input.lines().collect::<Vec<&str>>();
     ordered_signals.sort_by(|left,right| compare(left.as_bytes(), right.as_bytes()));
-    //println!("tabl {:?}", ordered_signals);
     let position2 = ordered_signals.iter().position(|l| l.eq(&"[[2]]")).unwrap() as i64 + 1;
     let position6 = ordered_signals.iter().position(|l| l.eq(&"[[6]]")).unwrap() as i64 + 1;
-
     let result = position2*position6;
     Ok(result)
 }
@@ -45,92 +43,21 @@ fn isrightorder(pair: &str) -> bool {
 // retourne Ordering::Equals si on doit continuer l'analyse
 // retourne Ordering::Greater si c'est pas dans le bon ordre
 fn compare(left: &[u8], right: &[u8]) -> Ordering {
-    if left[0] == b'[' && right[0] == b'[' {//gestion 2 listes
-        let mut i = 1;
-        let mut j = 1;
-        while left[i] != b']' && right[j] != b']' {
-            let l = left[i];
-            let r = right[j];
-            //on incrémente dès le début, ça nous évite de dupliquer ce code à chaque fois qu'on appelle continue;
-            //et si besoin d'accéder à la valeur de i, ou j, on fait i-1 et j-1
-            i += 1;
-            j += 1;
-
-            //analyse des 2 premiers caractères
-            match (l,r) {
-                (b'0'..=b'A', b'0'..=b'A') => {
-                    if l == r {continue;}
-                    else {return l.cmp(&r); }
-                },
-
-                (b',', b',') => continue,
-
-                (b'[', b'[') => {
-                    let subleft = sub_list(&left[i-1..]);
-                    let subright = sub_list(&right[j-1..]);
-                    let sousresultat = compare(subleft, subright);
-                    if sousresultat == Ordering::Equal {
-                        i += subleft.len()-1;
-                        j += subright.len()-1;
-                        continue;
-                    }
-                    else {return sousresultat;}
-                },
-
-                (b'[', b'0'..=b'A') => {
-                    let subleft = sub_list(&left[i-1..]);
-                    let subright = [b'[', r, b']'];
-                    let sousresultat = compare(subleft, &subright);
-                    if sousresultat == Ordering::Equal {
-                        i += subleft.len()-1;
-                        continue;
-                    }
-                    else {return sousresultat;}
-                },
-
-                (b'0'..=b'A', b'[') => {
-                    let subleft = [b'[', l, b']'];
-                    let subright = sub_list(&right[j-1..]);
-                    let sousresultat = compare(&subleft, subright);
-                    if sousresultat == Ordering::Equal {
-                        j += subright.len()-1;
-                        continue;
-                    }
-                    else {return sousresultat;}
-                },
-                _ => panic!("unknow chars")
-            }
-        }
-        //left side ran out of items
-        if left[i] == b']' && right[j] != b']' {
-            return Ordering::Less;
-        }
-        //right side ran out of items
-        if left[i] != b']' && right[j] == b']' {
-            return Ordering::Greater;
-        }
+    //analyse des 2 premiers caractères
+    match (left[0],right[0]) {
+        (a,b) if a==b => compare(&left[1..], &right[1..]),
+        (_, b']') => Ordering::Greater,
+        (b']', _) => Ordering::Less,
+        (b'[', _) => {
+            let subright = [&[right[0], b']'],&right[1..]].concat();
+            compare(&left[1..], &subright)
+        },
+        (_, b'[') => {
+            let subleft = [&[left[0], b']'],&left[1..]].concat();
+            compare(&subleft, &right[1..])
+        },
+        (_,_) => left[0].cmp(&right[0])
     }
-    Ordering::Equal
-}
-
-/**
- * search for closing ']'
- * attention : dans les cas où l'on a [1,2,[3,[4]]]
- * on risquerai de s'arrêter ici ---------------^
- * il faut donc compter également le nombre de '[' ouvrant
- */
-fn sub_list(substr: &[u8]) -> &[u8] {
-    let mut cout_bracket=1;
-    for i in 1..substr.len() {
-        if substr[i] == b'[' { cout_bracket += 1;}
-        if substr[i] == b']' {
-            cout_bracket -= 1;
-            if cout_bracket == 0 {
-                return &substr[..i+1];
-            }
-        }
-    }
-    substr
 }
 
 

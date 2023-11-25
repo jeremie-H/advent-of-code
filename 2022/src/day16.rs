714e106b966a7f25acf762521ff4286659cd0d01
jeremie-H <jeremie-H@users.noreply.github.com>
2023-11-25 01:25:13 +0100

    ✨ 2022 - day16

--- 2022/src/day16.rs
+++ 2022/src/day16.rs
@@ -14,7 +14,16 @@ pub fn part2(_input: &str) -> Result<i64, Box<dyn Error>> { Ok(0) }
 mod tests {
     use super::*;
 
-    const ÉNONCÉ: &str = "";
+    const ÉNONCÉ: &str = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II";
 
     #[test]
     fn test_part1() {

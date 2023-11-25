5196e700acec7fcfb849a19fa56197adfbcae5c9
jeremie-H <jeremie-H@users.noreply.github.com>
2023-11-25 01:09:38 +0100

    ✨ 2015 - day10

--- 2015/src/day10.rs
+++ 2015/src/day10.rs
@@ -1,38 +1,57 @@
+use std::collections::HashMap;
 use std::error::Error;
-
-use itertools::Itertools;
 
 /**
  * Part 1
  */
 pub fn part1(input: &str) -> Result<i64, Box<dyn Error>> {
-    let mut iter = input.chars();
-    iter.coalesce(|c1,c2| {
-        if c1 ==  c2 {
+    let mut input = String::from(input);
+    spell_digits(&mut input, 40);
+    Ok(input.len() as i64)
+}
 
+fn spell_digits(input: &mut String, loopnb: isize) {
+    let mut table: HashMap<String, String> = HashMap::new();
+
+    // Read initial input from user
+    table.insert(input.trim().to_string(), input.trim().to_string());
+
+    for _i in 0..loopnb {
+        // Generate the new string based on the input digits
+        let mut new_string = String::new();
+        let mut digit_count = 0;
+        let mut last_digit = ' ';
+        for c in input.trim().chars() {
+            if c == last_digit {
+                digit_count += 1;
+            } else {
+                if digit_count > 0 {
+                    new_string.push_str(&digit_count.to_string());
+                    new_string.push(last_digit);
+                }
+                last_digit = c;
+                digit_count = 1;
+            }
         }
-    });
-    // let mut count = 1;
-    // let mut previous = '-';
-    // (1..40).for_each(|_|{
-    // for c in iter.next()  {
-    //     println!("{}", c);
-    //     if previous == c {
-    //         count+=1;
-            
-    //     }else {
-    //         count = 1;
-    //         break;
-    //     }
-    //     previous= c;
-    // }});
-    Ok(0)
+        if digit_count > 0 {
+            new_string.push_str(&digit_count.to_string());
+            new_string.push(last_digit);
+        }
+
+        // Print the new string and use it as the input for the next iteration
+        //println!("{}", new_string);
+        *input = new_string;
+    }
 }
 
 /**
  * Part 2
  */
-pub fn part2(_input: &str) -> Result<i64, Box<dyn Error>> { Ok(0) }
+pub fn part2(input: &str) -> Result<i64, Box<dyn Error>> {
+    let mut input = String::from(input);
+    spell_digits(&mut input, 50);
+    Ok(input.len() as i64)
+}
 
 #[cfg(test)]
 mod tests {
@@ -40,18 +59,18 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1("1").unwrap(), 2);
+        assert_eq!(part1("1").unwrap(), 82350);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2("").unwrap(), 0);
+        assert_eq!(part2("1").unwrap(), 1166642);
     }
 
     #[test]
     fn real_tests() {
         // avec les vraies données
-        // assert_eq!(part1(include_str!("../inputs/input10.txt")).unwrap(), 0);
-        // assert_eq!(part2(include_str!("../inputs/input10.txt")).unwrap(), 0);
+        assert_eq!(part1(include_str!("../inputs/input10.txt")).unwrap(), 329356);
+        assert_eq!(part2(include_str!("../inputs/input10.txt")).unwrap(), 4666278);
     }
 }

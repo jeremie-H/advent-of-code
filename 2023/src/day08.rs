b4f65303a49e926ba1425878f2a1fbbef409cc93
jeremie-H <jeremie-H@users.noreply.github.com>
2023-12-08 18:31:57 +0100

    ✨ 2023 - day08

--- 2023/src/day08.rs
+++ 2023/src/day08.rs
@@ -10,7 +10,6 @@ fn load_data(input: &str) -> (&str, HashMap<&str, (&str,&str)>) {
         map.insert(&l[..3], (&l[7..10], &l[12..15]));
         map
     });
-    
     (directions, map)
 }
 
@@ -42,14 +41,53 @@ pub fn part1(input: &str) -> Result<i64, Box<dyn Error>> {
 /**
  * Part 2
  */
-pub fn part2(_input: &str) -> Result<i64, Box<dyn Error>> { Ok(0) }
+pub fn part2(input: &str) -> Result<i64, Box<dyn Error>> {
+    let (directions, map) = load_data(input);
+    let debuts = map.keys().filter(|s|s.as_bytes()[2] == b'A').collect_vec();
+    let r = directions.chars()
+    .cycle()
+    .fold_while((1, debuts), |(i, mut tableau), direction| {
+        let mut fin = true;
+        println!("tab : {:?}", tableau);
+        fin = match direction {
+            'R' => {
+                for i in 0..tableau.len() {
+                    
+                    tableau[i] = &map.get(tableau[i]).unwrap().1;
+                    fin &= tableau[i].as_bytes()[2] == b'Z';
+                    
+                }
+                fin
+            },
+            'L' => {
+                for i in 0..tableau.len() {
+                    
+                    tableau[i] = &map.get(tableau[i]).unwrap().0;
+                    fin &= tableau[i].as_bytes()[2] == b'Z';
+                }
+                fin
+            }
+            _ => panic!("direction inconnue")
+        };
+        if fin {
+            Done((i, tableau))
+        }
+        else {
+            Continue((i + 1, tableau))
+        }
+    }).into_inner().0;
+    // println!("map : {:?}", map);
+    // println!("r : {:?}", r);
+    Ok(r as i64)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const ÉNONCÉ: &str = "\
-RL
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("RL
 
 AAA = (BBB, CCC)
 BBB = (DDD, EEE)
@@ -57,12 +95,8 @@ CCC = (ZZZ, GGG)
 DDD = (DDD, DDD)
 EEE = (EEE, EEE)
 GGG = (GGG, GGG)
-ZZZ = (ZZZ, ZZZ)";
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1(ÉNONCÉ).unwrap(), 2);
-        assert_eq!(part1("\
-LLR
+ZZZ = (ZZZ, ZZZ)").unwrap(), 2);
+        assert_eq!(part1("LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
@@ -71,7 +105,16 @@ ZZZ = (ZZZ, ZZZ)").unwrap(), 6);
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(ÉNONCÉ).unwrap(), 0);
+        assert_eq!(part2("LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)").unwrap(), 6);
     }
 
     #[test]

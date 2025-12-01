bc554a1eb734fba42d32f27c663621b242c2d723
jeremie-H <jeremie-H@users.noreply.github.com>
2025-12-01 11:02:15 +0100

    chore: add day05

--- 2024/src/day05.rs
+++ 2024/src/day05.rs
@@ -14,14 +14,43 @@ pub fn part2(_input: &str) -> Result<i64, Box<dyn Error>> { Ok(0) }
 mod tests {
     use super::*;
 
+    const ÉNONCÉ: &str = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+
     #[test]
     fn test_part1() {
-        assert_eq!(part1("").unwrap(), 0);
+        assert_eq!(part1(ÉNONCÉ).unwrap(), 143);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2("").unwrap(), 0);
+        assert_eq!(part2(ÉNONCÉ).unwrap(), 0);
     }
 
     #[test]
